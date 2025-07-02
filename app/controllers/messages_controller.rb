ce950b201f078ff6e4c16b274693463593355176
TF-Meeta <takahiro.f9135@gmail.com>
2025-07-02 15:56:06 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,14 +2,16 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
 
   def create
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
     if @message.save
-      redirect_to room.messages.new(message_params)
+      redirect_to room_messages.new(message_params)
     else
+      @messages = @room.messages.includes(:user)
       render :index, status: :unprocessable_entity
     end
   end
